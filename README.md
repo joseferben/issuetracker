7b4b1fbddc16e7572d33539d4247340b1ca174fe
Josef Erben <josef@erben.io>
2016-10-10 21:18:36 +0200

    Adding readme.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # issuetracker
 This is an issue tracker made during the course WEB3 at ZHAW.
+
+# How to build
+You need to install webpack *npm i webpack -g*, then run *npm i*. 
+
+# How to serve
+Run *npm run serve* to run the webpack-dev-server or *npm run serveBad* to serve the static files with a basic http server.
--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,9 @@ module.exports = {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
                 loader: 'url?limit=10000&mimetype=image/svg+xml'
             },
-            { test: /jquery/, loader: 'expose?$!expose?jQuery' }
-
+            {
+                test: /jquery/, loader: 'expose?$!expose?jQuery'
+            }
         ]
     }
 };
