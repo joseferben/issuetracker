352d90a7a26b84ffa94ae7aef57a950076439c51
Josef Erben <josef@erben.io>
2016-11-05 14:42:31 +0100

    Add riotjs hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,59 @@
 var webpack = require("webpack");
 module.exports = {
-    entry: ["./entry.js"],
-    output: {
-        path: "./app",
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            },
-            {
-                test: /\.html$/,
-                loader: "raw-loader"
-            },
-            {
-                test: /\.png$/,
-                loader: "url-loader?limit=100000"
-            },
-            {
-                test: /\.jpg$/,
-                loader: "file-loader"
-            },
-            {
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/octet-stream'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=image/svg+xml'
-            },
-            {
-                test: /jquery/, loader: 'expose?$!expose?jQuery'
-            }
-        ]
-    }
+	entry: ["./entry.js"],
+	output: {
+		path: "./app",
+		filename: "bundle.js"
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			riot: 'riot'
+		})
+	],
+	module: {
+		preLoaders: [
+			{ test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' } }
+		],
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: "style-loader!css-loader"
+			},
+			{
+				test: /\.html$/,
+				loader: "raw-loader"
+			},
+			{
+				test: /\.png$/,
+				loader: "url-loader?limit=100000"
+			},
+			{
+				test: /\.jpg$/,
+				loader: "file-loader"
+			},
+			{
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=application/font-woff'
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=application/octet-stream'
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file'
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url?limit=10000&mimetype=image/svg+xml'
+			},
+			{
+				test: /jquery/, loader: 'expose?$!expose?jQuery'
+			},
+			{ 
+				test: /\.js$|\.tag$/, exclude: /node_modules/, loader: 'babel-loader'
+			}
+
+		]
+	}
 };
