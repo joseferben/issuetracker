8f569364629967dc03adf3204c555efc2137e618
Josef Erben <josef@erben.io>
2016-11-13 20:52:48 +0100

    Remove manual includes

--- entry.js
+++ entry.js
@@ -1,6 +1,7 @@
 import 'jquery';
 import 'bootstrap-loader';
 import 'eonasdan-bootstrap-datetimepicker';
+import riot from 'riot';
 import './src/datepicker';
 import './mockup.html';
 import './app/index.html';
