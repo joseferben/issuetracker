67c3f72faf37bb2afa3211315e3970f69283bdd7
Josef Erben <josef@erben.io>
2016-12-10 19:18:38 +0100

    fix typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ module.exports = {
 	app: function () {
 		const app = express()
 		const indexPath = path.join(__dirname, './app/index.html')
-		const publicPath = express.static(path.join(__dirname, './app'))
+		const publicPath = express.static(path.join(__dirname, '?'))
 
 		app.use('/', publicPath)
 		app.get('/', function (_, res) { res.sendFile(indexPath) })
