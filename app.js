e03b5188aaaf7179c2c6d8883a827ddcdaf877d5
Josef Erben <josef@erben.io>
2017-01-07 12:24:55 +0100

    Fix eslint issues

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const Server = require('./server.js')
-const port = (process.env.PORT || 8080)
-const app = Server.app()
+const Server = require('./server.js');
+const port = (process.env.PORT || 8080);
+const app = Server.app();
 
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const config = require('./webpack.config.js')
-const compiler = webpack(config)
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
 
-app.use(webpackHotMiddleware(compiler))
+app.use(webpackHotMiddleware(compiler));
 app.use(webpackDevMiddleware(compiler, {
       noInfo: true,
       publicPath: config.output.publicPath
-}))
+}));
 
-app.listen(port)
-console.log(`Listening at http://localhost:${port}`)
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
